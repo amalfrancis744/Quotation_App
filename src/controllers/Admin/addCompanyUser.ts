9009b7755aaaf54801c933d281c2635f2414add0
amalfrancis744 <amal.francis@dignizant.com>
2025-01-28 10:43:45 +0530

    company user creation add by admin side

---
+++ src/controllers/Admin/addCompanyUser.ts
@@ -0,0 +1,98 @@
+import bcrypt from "bcrypt";
+import { Response, Request, NextFunction } from "express";
+import { GlobleResponse } from "../../utils/response";
+import httpStatus from "http-status";
+import { ERROR_MSGS, INFO_MSGS } from "../../utils/constant";
+import * as userRespository from "../../repository/user.Repository";
+import * as companyRepository from "../../repository/company.Repository";
+import mongoose from "mongoose";
+
+export const AddCompanyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { firstName, lastName, email, password, companyId } = req.body;
+
+
+    // Check for missing credentials
+    if (!firstName || !lastName || !email || !password || !companyId) {
+      return GlobleResponse.error({
+        res,
+        status: httpStatus.BAD_REQUEST,
+        msg: "Credentials are missing",
+      });
+    }
+    if(!mongoose.Types.ObjectId.isValid(companyId)){
+
+        return GlobleResponse.error({
+            res,
+            status: httpStatus.BAD_REQUEST,
+            msg: "Invalid Company ID",
+        })
+    }
+
+    // Check if email already exists
+    const existingUser = await userRespository.findUserByEmail(email);
+    if (existingUser) {
+      return GlobleResponse.error({
+        res,
+        status: httpStatus.BAD_REQUEST,
+        msg: ERROR_MSGS.EMAIL_EXISTS,
+      });
+    }
+
+
+    // check companyID is Valid
+   const existingCompany = await  companyRepository.findCompanyById(companyId)
+    if(!existingCompany){
+        return GlobleResponse.error({
+            res,
+            status: httpStatus.BAD_REQUEST,
+            msg: "Company not found the given ID",
+        })
+
+    }
+  
+    // Check if the companyId is already associated with another user
+    const existingCompanyUser = await userRespository.findUserByCompanyId(
+      companyId
+    );
+    
+    if (existingCompanyUser) {
+      return GlobleResponse.error({
+        res,
+        status: httpStatus.BAD_REQUEST,
+        msg: "This company is already associated with another user.",
+      });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create the new user
+    const newUser = await userRespository.createUser({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      companyId,
+    });
+
+    return GlobleResponse.success({
+      res,
+      status: httpStatus.CREATED,
+      msg: INFO_MSGS.SUCCESSFUL_REGISTER,
+      data: newUser,
+    });
+  } catch (error) {
+    console.error("Error in RegisterUser:", error);
+    return GlobleResponse.error({
+      res,
+      status: httpStatus.INTERNAL_SERVER_ERROR,
+      msg: "An error occurred while registering the user.",
+    });
+  }
+};
--- src/routes/Admin/admin.routes.ts
+++ src/routes/Admin/admin.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as adminController from "..//../controllers/Admin/adminAuth";
 import * as companyController from "..//../controllers/Admin/company";
+import * as companyUserController from "..//../controllers/Admin/addCompanyUser"
 
 const router = express.Router();
 
@@ -14,5 +15,9 @@ router.get("/companies", companyController.getAllCompanies);
 router.get("/companies/:company_id", companyController.getCompanyById);
 router.put("/companies/:company_id", companyController.updateCompany);
 router.delete("/companies/:company_id",companyController.deleteCompany)
+
+
+// create companyUser by admin
+router.post("/companies/addUser", companyUserController.AddCompanyUser)
 
 export default router;
